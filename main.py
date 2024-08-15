364e3c3782b6f7a35648bb23cb07a4e14f281e8d
George <110062762+georgebarr@users.noreply.github.com>
2024-08-15 21:12:56 +0100

    outlined program

---
+++ main.py
@@ -0,0 +1,26 @@
+from typing import Dict
+
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+def fetch_score(text: str):
+
+    # Returns polarity score
+
+    pass
+
+
+def calculate_mood(polarity_score: Dict[str, float]):
+
+    # returns a simplified form of the mood based on the polarity score
+
+    pass
+
+
+def main():
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
